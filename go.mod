627e4cd555acb30f615c19936fde80df31bb56cf
jacktaoyang <jacktaoyang@tencent.com>
2022-12-27 14:45:34 +0800

    feat:add rabbitmq

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/streadway/amqp v1.0.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,36 +1,10 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"golang.org/x/sync/errgroup"
-	"log"
-	"time"
+	mq "learn_code/rabbit_mq"
 )
 
 func main() {
-	eg, ctx := errgroup.WithContext(context.Background())
-
-	for i := 0; i < 100; i++ {
-		i := i
-		eg.Go(func() error {
-			time.Sleep(2 * time.Second)
-
-			select {
-			case <-ctx.Done():
-				fmt.Println("Canceled:", i)
-				return nil
-			default:
-				if i > 90 {
-					fmt.Println("Error:", i)
-					return fmt.Errorf("error: %d", i)
-				}
-				fmt.Println("End:", i)
-				return nil
-			}
-		})
-	}
-	if err := eg.Wait(); err != nil {
-		log.Fatalln(err)
-	}
+	mq.Send("hello", "hello world")
+	mq.Receive("hello")
 }
